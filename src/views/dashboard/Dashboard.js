6570a0466b52399534d32a12ae8dcd8d5e1b94e0
itsmanojb <the_manoj@live.com>
2020-06-20 01:17:48 +0530

    :sparkles: static sidenav added

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -8,6 +8,7 @@ import { Alert } from '../../common/alert/Alert';
 import { Loader } from '../../common/loader/Loader';
 
 import './Dashboard.scss';
+import SideNav from './Sidenav';
 
 export const Dashboard = () => {
 
@@ -36,29 +37,37 @@ export const Dashboard = () => {
         <Loader />
       ) : (
           <main className="content">
-            <div className="board-header">
-              <h2>Boards</h2>
-            </div>
-            <div className="overflow-scroll">
-              {boards.length === 0 && (
-                <Alert type="info" isClosable={false}>
-                  You haven't created any boards. Kindly click on the 'Create a
-                  Board' button in the navigation bar to create a board.
-                </Alert>
-              )}
-              <div className="boards">
-                {boards.map((board) => {
-                  return (
-                    <Link
-                      to={'/board/' + board.id}
-                      key={board.id}
-                      className="board"
-                    >
-                      <div>{board.name}</div>
-                    </Link>
-                  );
-                })}
+            <div className="dashboard">
+              <div>
+                <SideNav />
               </div>
+              <div className="all-boards">
+                <div className="board-header">
+
+                </div>
+                <div className="boards-listing">
+                  {boards.length === 0 && (
+                    <Alert type="info" isClosable={false}>
+                      You haven't created any boards. Kindly click on the 'Create a
+                      Board' button in the navigation bar to create a board.
+                    </Alert>
+                  )}
+                  <div className="boards">
+                    {boards.map((board) => {
+                      return (
+                        <Link
+                          to={'/board/' + board.id}
+                          key={board.id}
+                          className="board"
+                        >
+                          <div>{board.name}</div>
+                        </Link>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="sidebar right"></div>
             </div>
           </main>
         )}
---
+++ src/views/dashboard/Sidenav.js
@@ -0,0 +1,46 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React from 'react';
+import Icon from '../../components/misc/IonIcon';
+import './Sidenav.scss';
+
+const SideNav = () => {
+  return (
+    <div className="sidenav">
+      <ul className="sidenav-nav">
+        <li className="nav-item">
+          <a className="nav-link">
+            <Icon name="grid-outline" />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <Icon name="analytics-outline" />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <Icon name="calendar-outline" />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <Icon name="basketball-outline" />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <Icon name="folder-outline" />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <Icon name="power" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SideNav;
